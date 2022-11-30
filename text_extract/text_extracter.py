f6c230f1a081729fb7cc912d17a2205b12e8dccf
Monika Singh <32133030+monika2910@users.noreply.github.com>
2022-11-30 00:42:18 +0000

    Add files via upload

---
+++ text_extract/text_extracter.py
@@ -0,0 +1,230 @@
+from distutils.log import error
+from typing import Optional
+import psutil
+
+import tika
+#tika.initVM()
+
+from tika import parser
+import os
+import re
+import pdf2image
+import pytesseract
+from pytesseract import Output, TesseractError
+import os
+import shutil
+
+# define the resume folder paths
+# resume_source = "./resumes/01_resume_source"
+# resume_extracted = "./resumes/02_resume_extracted"
+# resume_cleaned = "./resumes/04_resume_cleaned"
+# resume_not_extracted = "./resumes/03_resume_not_extracted"
+
+class text_extracter:
+
+    def __init__(self, resume_source, resume_extracted, resume_cleaned, resume_not_extracted):
+        self.resume_source = resume_source
+        self.resume_extracted = resume_extracted
+        self.resume_cleaned = resume_cleaned
+        self.resume_not_extracted = resume_not_extracted
+
+
+    '''
+    text_extraction_image_pdf(resume_file_path)
+    Feature: Extract text data from pdf docuemnts created from image file and save it in a text file.
+    Input: File path source, file path destination
+    Exception: Image size up to the limit of 178956970 pixels
+    '''
+
+    def text_extraction_image_pdf(self, resume_file_path):
+        pdf_path = resume_file_path
+        pTextString = ""
+        if (pdf_path.endswith(".pdf")):
+            try:
+                print("Attempt to Extract Text from image PDF File : ", pdf_path)
+                images = pdf2image.convert_from_path(pdf_path)
+                pTotalPages = (len(images))
+
+                for x in range(pTotalPages):
+                    text = ""
+                    pil_im = images[x]
+                    ocr_dict = pytesseract.image_to_data(pil_im, lang='eng', output_type=Output.DICT)
+                    # ocr_dict now holds all the OCR info including text and location on the image
+                    text = " ".join(ocr_dict['text'])
+                    pTextString = pTextString + "\n" + "\n" + "\n" + text
+                    # print(pTextString)
+                    # return pTextString
+            except Exception as e:
+                print(e)
+        return pTextString
+
+    '''
+    text_extraction(resume_source,resume_extracted)
+    Feature: Extract data from pdf, docx, doc and rtf files and save it in a text file.
+    Input: File path source, file path destination
+    Exception: pdfs created with images are not extracted
+    '''
+
+    def text_extraction(self):
+
+        try:
+
+            '''
+            tika process is running on a java server. The function will check the status of tika process
+            '''
+
+            def get_tika_process() -> Optional[psutil.Process]:
+                for process in psutil.process_iter(["name", "cmdline"]):
+                    if "java" in process.name():
+                        for part in process.cmdline():
+                            if "tika" in part:
+                                return process
+
+            # to stop running process before stating a new one.
+            existing_tika_process = get_tika_process()  # Changed for Python 3.6
+            if (existing_tika_process):
+                # print("Found tika process:", existing_tika_process)
+                # print("Existing process args:", existing_tika_process.cmdline())
+                existing_tika_process.terminate()
+                terminate_result = existing_tika_process.wait(10)
+                print(f"Terminated tika; exit code {terminate_result}\n")
+            else:
+                print("No existing tika process found\n")
+
+            #tika_server.TikaJavaArgs += "-Xmx1G"
+
+            print("\nStart Resume Extraction")
+            print("Source Folder :\n")
+
+            # Loop through the files inside the source folder. Extract text using the tika.
+            for process_file in os.listdir(self.resume_source):
+                file, extension = os.path.splitext(process_file)
+                pInvalidPDFFile = False
+                pInvalidFile = False
+                try:
+                    resume_file_path = os.path.join(self.resume_source, process_file)
+
+                    print(resume_file_path)
+                    print(process_file)
+                    #parsed = parser.from_file("C:/Users/44785/INeuron_classes/NLP_ineuron_Notes_Projects/resume_ner_monika/resumes/prediction/01_resume_source")
+                    parsed = parser.from_file(resume_file_path)
+
+                    print("Tika server started")
+                    new_tika_process = get_tika_process()
+                    if new_tika_process:
+                        # print("New process args:", new_tika_process.cmdline())
+                        new_tika_process.cmdline()
+
+                    # print(parsed["metadata"])        # for getting the file metadata
+                    pTextString = parsed["content"]  # for getting the file content
+                    # print(pTextString)
+                    try:
+                        pTextStringLength = len(pTextString)
+                        print(pTextStringLength)
+                        if (pTextStringLength < 1000):
+                            print("Note : Total extracted charecters are less than 1000")
+                            if (resume_file_path.endswith(".pdf")):
+                                pInvalidPDFFile = True
+                            else:
+                                pInvalidFile = True
+
+                    except:
+                        print("Note : Extractied Text object is of type NoneType")
+                        if (resume_file_path.endswith(".pdf")):
+                            pInvalidPDFFile = True
+                        else:
+                            pInvalidFile = True
+
+                    if (pInvalidPDFFile):
+                        try:
+                            if (resume_file_path.endswith(".pdf")):
+                                pTextString = self.text_extraction_image_pdf(resume_file_path)
+                                pTextStringLength = len(pTextString)
+                                print("Image PDF Text Extracted : ", pTextStringLength)
+                                if (pTextStringLength < 1000):
+                                    pInvalidFile = True
+
+                        except Exception as e:
+                            print(e)
+
+                    if (pInvalidFile):
+                        try:
+                            presume_source_file_path = os.path.join(self.resume_source, process_file)
+                            shutil.copy2(presume_source_file_path, self.resume_not_extracted)
+                            print("Copied the non processed file to the folder")
+
+                        except Exception as e:
+                            print(e)
+                    else:
+                        try:
+                            # Write the extracted text data in to a file
+                            resume_extracted_file_path = os.path.join(self.resume_extracted, process_file)
+                            generatedFile = resume_extracted_file_path + ".txt"
+
+                            # pTextString = pTextString.encode('utf-8')
+                            # file = open(generatedFile, "w")
+                            # file.write(pTextString)
+                            # file.close()
+                            with open(generatedFile, 'w', encoding="utf-8") as f:
+                                f.write(pTextString)
+
+                        except Exception as e:
+                            print(e)
+
+                except Exception as e:
+                    print(e)
+
+        except Exception as e:
+            print(e)
+
+    '''
+    text_cleaning(resume_extracted,resume_cleaned):
+    Feature: Clean the text on the file. Currently removing the multiple newline and multiple newline with trailing spaces from the text. 
+    Input: File path source, file path destination
+    Exception: Special charecters are currently not removed
+    '''
+
+    def text_cleaning(self):
+        try:
+            print("\nStart Resume Cleaning")
+            print("Source Folder : \n")
+            for process_file in os.listdir(self.resume_extracted):
+                file, extension = os.path.splitext(process_file)
+                try:
+                    resume_extracted_file_path = os.path.join(self.resume_extracted, process_file)
+                    print(resume_extracted_file_path)                    
+
+                    resume_cleaned_file_path = os.path.join(self.resume_cleaned, process_file)
+                    generatedFile_cleaned = resume_cleaned_file_path
+
+                    with open(resume_extracted_file_path, 'r', encoding="utf-8") as f:
+                        text_data = f.read()
+
+                    # print(text_data)
+
+                    def remove_multiple_new_line(text):
+
+                        # Replcace multiple new line with trailing spaces with a new line
+                        pattern_multiple_new_line_withSpace = r'\n+\s+'
+                        p_text_result01 = re.sub(pattern_multiple_new_line_withSpace, '\n', text).strip()
+
+                        # Replcace multiple new line with a new line
+                        pattern_multiple_new_line = r'\n+'
+                        p_text_result02 = re.sub(pattern_multiple_new_line, '\n', p_text_result01).strip()
+
+                        return p_text_result02
+
+                    clean_text = remove_multiple_new_line(text_data)
+
+                    pclean_textLength = len(clean_text)
+                    print(pclean_textLength)
+
+                    # print(clean_text)
+
+                    with open(generatedFile_cleaned, 'w', encoding="utf-8") as f:
+                        f.write(clean_text)
+
+                except Exception as e:
+                    print(e)
+        except Exception as e:
+            print(e)
---
+++ text_preprocessing/text_preprocess.py
@@ -0,0 +1,115 @@
+import unicodedata
+import re
+import os
+# import nltk
+# import nltk.data 
+# import string
+# import spacy
+# import contractions
+# from nltk.tokenize import ToktokTokenizer
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+
+# define the resume folder paths
+# resume_cleaned_folder_path = "./resumes/04_resume_cleaned"
+# pre_processed_folder_path = "./resumes/05_resume_pre_processed/"
+
+class text_preprocess:
+
+    def __init__(self, resume_cleaned_folder_path, pre_processed_folder_path):
+        self.resume_cleaned_folder_path = resume_cleaned_folder_path
+        self.pre_processed_folder_path = pre_processed_folder_path
+
+    # function to remove accented characters
+    def remove_accented_chars(self, text):
+        new_text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+        return new_text
+
+    # function to convert to lower case characters
+    def to_lowercase(self, text):
+        return text.lower()
+
+        # function to replace new line with .nline
+
+    def replace_newline(self, text):
+        pattern02 = r'\n'
+        return re.sub(pattern02, ' .nline ', text).strip()
+
+    # function to remove special characters
+    def remove_SpecialCharecters(self, text):
+        pattern = r'[^a-zA-Z0-9 .@\\/:,]'
+        return re.sub(pattern, '', text).strip()
+
+        # function to remove white space characters
+
+    def remove_extra_whitespace_tabs(self, text):
+        pattern = r'^\s*|\s\s*'
+        test_00 = re.sub(pattern, ' ', text).strip()
+        return test_00
+
+    # # function to lemma
+    # def get_lem(text):
+    #     nlp = spacy.load('en_core_web_sm')
+    #     text = nlp(text)
+    #     text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+    #     return text
+
+    # # function to remove Stop words
+    # def remove_stopwords(text):
+    #     stopword_list = nltk.corpus.stopwords.words('english')
+    #     tokenizer = ToktokTokenizer()
+    #     # convert sentence into token of words
+    #     tokens = tokenizer.tokenize(text)
+    #     tokens = [token.strip() for token in tokens]
+    #     # check in lowercase
+    #     t = [token for token in tokens if token.lower() not in stopword_list]
+    #     text = ' '.join(t)
+    #     return text
+
+    # # function to expand contractions
+    # def expand_contractions(text):
+    #     return contractions.fix(text)
+
+    def preprocess(self, source_path):
+        try:
+            print(f"\nPre-process started for File: {source_path}")
+            with open(source_path, encoding='utf8') as f:
+                input_text = f.readlines()
+                text = "".join(input_text)
+
+            text_remove_accented_chars = self.remove_accented_chars(text)
+            text_to_lower = self.to_lowercase(text_remove_accented_chars)
+            text_with_nline = self.replace_newline(text_to_lower)
+            text_withOut_specialCharecters = self.remove_SpecialCharecters(text_with_nline)
+            text_withOut_whitespace = self.remove_extra_whitespace_tabs(text_withOut_specialCharecters)
+
+            # text_lem = get_lem(text)
+            # text_stopwords = remove_stopwords(text_lem)
+            # text_contraction = expand_contractions(text_stopwords)
+
+            f.close()
+
+        except Exception as e:
+            print("ERROR : preprocess", e)
+
+        return text_withOut_whitespace
+
+    def preprocess_text(self):
+        print("\n Start Preprocessing the files.. \n")
+        try:
+            for files in os.listdir(self.resume_cleaned_folder_path):
+                full_path = os.path.join(self.resume_cleaned_folder_path, files)
+
+                preprocess_file = self.preprocess(full_path)
+
+                processed_file_path = self.pre_processed_folder_path + files
+                print(f"Destination File-Path:  {processed_file_path}")
+                out_file = open(processed_file_path, "w", encoding="utf-8")
+                out_file.write(str(preprocess_file))
+                out_file.close()
+                print("Preporcessed file is saved - " + processed_file_path)
+                print('\n')
+
+        except Exception as e:
+            print("ERROR : preprocess", e)
